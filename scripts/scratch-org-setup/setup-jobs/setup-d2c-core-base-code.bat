01a3c17c7f30e59398ea9d56c6a748c2b45bea85
mike.tierman@trustage.com <mike.tierman@trustage.com>
2023-10-30 17:09:15 -0500

    D2C Base Code Package

--- scripts/scratch-org-setup/setup-jobs/setup-d2c-core-base-code.bat
+++ scripts/scratch-org-setup/setup-jobs/setup-d2c-core-base-code.bat
@@ -1,7 +1,6 @@
 
 
-REM This script will install package version for D2C Core Base Configuration package.
-REM Test data is also generated along with test admin user account is created if selected.  
+REM This script will install package version for D2C Core Base code package. 
 REM  !!!!  This should only be used for scratch org setup.  !!!!!!!
 REM -------------------------------------------------------------------------------------------------------
 IF [%1] == [] goto Setup
@@ -17,7 +16,7 @@ set /p installationkey=Enter Installation Key to install:
 
 :Install
 echo Getting latest package version id for Package Id: 0HoDm0000004CEIKA2  - D2C Core Base Configurations
-call sf data query --query "SELECT SubscriberPackageVersionId FROM Package2Version WHERE Package2ID = '0HoDm0000004CEIKA2' ORDER BY CreatedDate DESC LIMIT 1" > c:\temp\packageVerID.json --use-tooling-api --result-format json --target-org %devhub% --wait 2' 
+call sf data query --query "SELECT SubscriberPackageVersionId FROM Package2Version WHERE Package2ID = '0HoDm000000blK7KAI' ORDER BY CreatedDate DESC LIMIT 1" > c:\temp\packageVerID.json --use-tooling-api --result-format json --target-org %devhub% --wait 2' 
 echo Getting json file from temp drive with results from query
 for /f "tokens=1,2 delims=:{} " %%A in (c:\temp\packageVerID.json) do (
     If "%%~A"=="SubscriberPackageVersionId" set packageversionid=%%~B 
@@ -26,9 +25,5 @@ for /f "tokens=1,2 delims=:{} " %%A in (c:\temp\packageVerID.json) do (
 echo Found Package Version ID %packageversionid%  Installing package 
 call sf package install --package %packageversionid% --installation-key %installationkey% --wait 2 --security-type AllUsers 
 echo "*** Assign Permission Sets ...."
-call sf org assign permset --name Admin_D2CBaseConfiguration_ModifyAll
+call sf org assign permset --name API_Connect_Principal_Access
 
-echo "*** Load data ... " 
-call sf data import tree  --plan scripts\scratch-org-setup\data\d2c-core-base-configuration-data-plan.json
-echo "*** Updating Account record type ..."
-call sf apex run -f scripts\scratch-org-setup\apex\D2CBaseConfig_UpdateAccountRecordtype.apex
---
+++ scripts/scratch-org-setup/setup-jobs/setup-install-all-packages.bat
@@ -0,0 +1,26 @@
+echo off
+REM ------------------------------------------------------------------------------------------------------
+REM This script will install all packages in the correct order for your scratch org.  
+REM Test data is also generated along with test admin user account is created if selected.  
+REM  !!!!  This should only be used for scratch org setup.  !!!!!!!
+REM -------------------------------------------------------------------------------------------------------
+
+set /p DevHub= Enter the alias for the authorized dev hub:
+
+REM Scratch Orgs may need profiles created before running packages.  Research if this can be scripted.
+REM For now Scratch orgs should be setup manually and profiles created before running this script
+:: set /p ScratchOrgYN= Have you already setup a scratch org (Y/N)
+set /p InstallationKey= Enter Installation Key for packages: 
+:: if /I %ScratchOrgYN% == Y goto :LoadSO
+
+:: set /p ScratchOrgName=Name of Scratch Org: 
+:: set /p NumberOfDays=Number of Days for Scratch Org:
+:: echo "*** Creating scratch Org..." %ScratchOrgName%
+:: call sf org create scratch --set-default --definition-file config/project-scratch-def.json --alias %ScratchOrgName%  --duration-days %NumberOfDays% --wait 10
+
+:LoadSO
+call sf project deploy start --source-dir "d2c-org/d2c-core/unpackaged-configurations"
+call scripts\scratch-org-setup\setup-jobs\setup-d2c-core-base-configuration.bat %DevHub% %InstallationKey%
+call scripts\scratch-org-setup\setup-jobs\setup-ilh-sales-base-configuration.bat %DevHub% %InstallationKey%
+call scripts\scratch-org-setup\setup-jobs\setup-d2c-core-base-code.bat %DevHub% %InstallationKey%
+
