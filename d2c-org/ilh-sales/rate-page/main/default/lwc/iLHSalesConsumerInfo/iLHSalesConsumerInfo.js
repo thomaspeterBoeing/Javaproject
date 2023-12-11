eaa9a8773f2560d3ae1744392369c9d4078ed168
Adam.Perschke-Ely <Adam.Perschke-Ely@trustage.com>
2023-12-11 10:06:51 -0600

    Updated test coverage

--- d2c-org/ilh-sales/rate-page/main/default/lwc/iLHSalesConsumerInfo/iLHSalesConsumerInfo.js
+++ d2c-org/ilh-sales/rate-page/main/default/lwc/iLHSalesConsumerInfo/iLHSalesConsumerInfo.js
@@ -5,44 +5,43 @@ import { reduceErrors } from 'c/ldsUtils';
 export default class ILHSalesConsumerInfo extends LightningElement {
     @api opportunityId = '006DS00000LZR0VYAX';
 
-    errorMessage = undefined;
-    accountName;
-    insurancePurpose;
-    healthAssessment;
-    tobaccoUse;
-    affiliation;
-    proposedAmount;
-    accountState;
+    errorMessage;
     opportunity;
 
     connectedCallback() {
         this.getOpportunityFields();
     }  
 
+    /**
+     * Purpose: This function gets opportunity fields
+     */
     getOpportunityFields() {
-        getOpportunityFieldsData({opportunityId : this.opportunityId}).then(data =>{				
+        getOpportunityFieldsData({opportunityId : this.opportunityId}).then(data =>{			
 			this.opportunity = data;	
 		}).catch(error =>{
 			this.errorMessage = reduceErrors(error);
 		});
     }
     
+    /**
+     * Getters
+     */
     get fullName() {
         let retStr = '';
         if(this.opportunity) {
-            if(this.opportunity.Account.Salutation) {
+            if(this.opportunity?.Account?.Salutation) {
                 retStr += ' ' + this.opportunity.Account.Salutation;
             }
-            if(this.opportunity.Account.FirstName) {
+            if(this.opportunity?.Account?.FirstName) {
                 retStr += ' ' + this.opportunity.Account.FirstName;
             }
-            if(this.opportunity.Account.MiddleName) {
+            if(this.opportunity?.Account?.MiddleName) {
                 retStr += ' ' + this.opportunity.Account.MiddleName;
             }
-            if(this.opportunity.Account.LastName) {
+            if(this.opportunity?.Account?.LastName) {
                 retStr += ' ' + this.opportunity.Account.LastName;
             }
-            if(this.opportunity.Account.Suffix) {
+            if(this.opportunity?.Account?.Suffix) {
                 retStr += ' ' + this.opportunity.Account.Suffix;
             }
         }
@@ -50,35 +49,35 @@ export default class ILHSalesConsumerInfo extends LightningElement {
     }
 
     get accountGender() {
-        return this.opportunity ? this.opportunity.Account.Gender__pc : '';
+        return this.opportunity?.Account?.Gender__pc;
     }
     
     get accountAge() {
-        return this.opportunity ? this.opportunity.Account.Age__pc : '';
+        return this.opportunity?.Account?.Age__pc;
     }
 
     get insurancePurpose() {
-        return this.opportunity ? this.opportunity.InsurancePurpose__c : '';
+        return this.opportunity?.InsurancePurpose__c;
     }
 
     get healthAssessment() {
-        return this.opportunity ? this.opportunity.HealthAssessment__c : '';
+        return this.opportunity?.HealthAssessment__c;
     }
 
     get tobaccoUse() {
-        return this.opportunity ? this.opportunity.TobaccoUse__c : '';
+        return this.opportunity?.TobaccoUse__c;
     }
 
     get affiliation() {
-        return this.opportunity ? this.opportunity.Affiliation__r.Name : '';
+        return this.opportunity?.Affiliation__r?.Name;
     }
 
     get proposedAmount() {
-        return this.opportunity ? this.opportunity.Amount : '';
+        return this.opportunity?.ProposedCoverage__c;
     }
 
     get accountState() {
-        return this.opportunity ? this.opportunity.Account.DomicileState__c : '';
+        return this.opportunity?.Account?.DomicileState__c;
     }
 }
 
