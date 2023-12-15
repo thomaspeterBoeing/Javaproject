fa6e1285db8d7f2c5d934ae132917e72ff22b978
Adam.Perschke-Ely <Adam.Perschke-Ely@trustage.com>
2023-12-15 13:16:26 -0600

    Updated cart

--- d2c-org/ilh-sales/rate-page/main/default/lwc/iLHSalesCart/iLHSalesCart.js
+++ d2c-org/ilh-sales/rate-page/main/default/lwc/iLHSalesCart/iLHSalesCart.js
@@ -1,31 +1,64 @@
 import { LightningElement, wire, track, api } from 'lwc';
 import { subscribe, MessageContext, unsubscribe } from 'lightning/messageService';
+import { refreshApex } from '@salesforce/apex';
 import RATE_PAGE_CHANNEL from '@salesforce/messageChannel/Rate_Page__c';
 import getQuotes from '@salesforce/apex/ILHCartController.getQuotes';
 import deleteQuote from '@salesforce/apex/ILHCartController.deleteQuote';
 import insertQuote from '@salesforce/apex/ILHCartController.insertQuote';
+import checkout from '@salesforce/apex/ILHCartController.checkout';
 import { reduceErrors } from 'c/ldsUtils';//LWC Reduces one or more LDS errors into a string[] of error messages
 
 export default class ILHSalesCart extends LightningElement {
-    @api recordId;
-    decision = '';
+    @api opportunityId = '006DR00000JKEbmYAH';
     @track cartData = [];
-    showSpinner = false;
+    wiredResult;
+    errorMessage = '';
+    decision = '';
+    showSpinner = true;//Spinner will turn off in getQuotes function
     totalCoverage = 0;
     totalCost = 0;
 
+    /**
+	 * Purpose: This function gets called when component is connected to page
+	 */
     connectedCallback() {
         this.subscribeToMessageChannel(); 
-        this.findQuotes();     
     }
     
+    /**
+	 * Purpose: This function gets called when component is disconnected from page
+	 */
     disconnectedCallback() {
         unsubscribe(this.subscription);      
     }
 
+    /**
+	 * Purpose: Wiring message context
+	 */
     @wire(MessageContext)
     messageContext;
 
+    /**
+     * Purpose: Calls APEX to find quotes for related opportunity
+     */
+    @wire(getQuotes, {oppId: '$opportunityId'})
+    getQuotes(value) {
+        this.wiredResult = value;
+        const { data, error } = this.wiredResult;
+        if (data) { 
+            this.cartData = data;
+            this.calculateTotals();
+            this.showSpinner = false;
+        }
+        else if (error) {
+            this.errorMessage = reduceErrors(error);
+            this.showSpinner = false;
+        }
+    }
+
+    /**
+     * Purpose: This function subscribes to rate page message channel
+     */
     subscribeToMessageChannel() {        
         this.subscription = subscribe(
             this.messageContext,
@@ -34,53 +67,54 @@ export default class ILHSalesCart extends LightningElement {
         );
     }
 
+    /**
+     * Purpose: This function creates a new quote object and calls insert quote function
+     * @param message : Message that was sent to rate page message channel
+     */
     handleMessage(message) {
         let newCartItem = {
-            id: "ci" + this.cartData.length + 1,
             productName: message.productName,
             coverage: message.coverage,
             cost: message.cost
         };
-        this.insertQuote();
+        this.insertQuote(newCartItem);
     }
 
-    findQuotes() {
-        this.showSpinner = true;
-        getQuotes({ oppId: this.recordId})
-        .then(response => {
-            this.cartData = response;
-            this.calculateTotals();
-            this.showSpinner = false;
-        }).catch(error => {
-            console.log(reduceErrors(error));
-            this.showSpinner = false;
-        });
-    }
-
+    /**
+     * Purpose: Calls APEX to delete quote record for related quote id
+     * @param event : Event from remove button
+     */
     deleteQuote(event) {
         this.showSpinner = true;
         deleteQuote({ quoteId: event.target.dataset.id})
         .then(response => {
-            this.findQuotes();
+            refreshApex(this.wiredResult);
             this.showSpinner = false;
         }).catch(error => {
-            console.log(reduceErrors(error));
+            this.errorMessage = reduceErrors(error);
             this.showSpinner = false;
         });
     }
 
-    insertQuote() {
+    /**
+     * Purpose: This function calls APEX to create a new quote record
+     * @param newCartItem : New cart item object to insert
+     */
+    insertQuote(newCartItem) {
         this.showSpinner = true;
-        insertQuote({ oppId: this.recordId})
+        insertQuote({ oppId: this.opportunityId})
         .then(response => {
-            this.findQuotes();
+            refreshApex(this.wiredResult);
             this.showSpinner = false;
         }).catch(error => {
-            console.log(reduceErrors(error));
+            this.errorMessage = reduceErrors(error);
             this.showSpinner = false;
         });
     }
 
+    /**
+     * Purpose: This method calculates grand total values
+     */
     calculateTotals() {
         this.totalCoverage = 0;
         this.totalCost = 0;
@@ -90,6 +124,18 @@ export default class ILHSalesCart extends LightningElement {
         }
     }
 
+    handleCheckout() {
+        checkout()
+        .then(response => {
+        }).catch(error => {
+            this.errorMessage = reduceErrors(error);
+        });
+    }
+
+    /**
+     * Purpose: Getting decision options to display in picklists for each quote record in the cart
+     * TODO: This will be moved to an APEX class where it will return option based on product and state
+     */
     get decisionOptions() {
         return [
             { label: '', value: '' },
