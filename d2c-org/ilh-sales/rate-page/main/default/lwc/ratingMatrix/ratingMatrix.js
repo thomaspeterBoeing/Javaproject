c3346ce8cc3b6307eef59f758d76bab37eebe118
Adam.Perschke-Ely <Adam.Perschke-Ely@trustage.com>
2024-03-11 15:58:33 -0500

    Cart message channel updates

--- d2c-org/ilh-sales/rate-page/main/default/lwc/ratingMatrix/ratingMatrix.js
+++ d2c-org/ilh-sales/rate-page/main/default/lwc/ratingMatrix/ratingMatrix.js
@@ -11,100 +11,57 @@
  * Modifications:
  *************************************************************************************/
 
-import { LightningElement,api,wire } from 'lwc';
-import { publish, MessageContext } from 'lightning/messageService';
-import CART_CHANNEL from '@salesforce/messageChannel/Cart__c';
-
+import { LightningElement, api } from 'lwc';
 export default class RatingMatrix extends LightningElement {
-        
     rateData = [];
-    rateColumns =[];
-    subscription = null;
+    rateColumns = [];
     payload;
-      
-    /**
-	    * Purpose: Wiring message context
-    */
-    @wire(MessageContext)
-    MessageContext;
 
-
-   @api buildTable(rates,products,frequency){
+    @api buildTable(rates, products, frequency) {
         this.rateData = [...rates];
-        this.rateColumns = []; 
-        let columns = [];       
-        const len = products.length;
+        this.rateColumns = [];
+        let columns = [];
+        const len = products?.length;
 
-        if(products != null && products.length > 0){ 
-     
-        columns.push(
-            {label: 'Coverage', fieldName: 'coverage', type :'currency', cellAttributes: { alignment: 'left' } }
-        )
-        for (let index = 0;  index < len; ++index){                
-
+        if (products != null && products?.length > 0) {
             columns.push(
-                {
-                    label: products[index], type :'cellselector',                             
-                        typeAttributes: {                                
-                            value: {fieldName: products[index]},   
-                            labelkey: frequency,
-                            labeltype: "currency",
-                            checkedvariant: "brand-outline",
-                            uncheckedvariant: "base",
-                            checkedicon: "action:approval",
-                            uncheckedicon: "",
-                            lockwhenselected: true,
-                            cellallignment: "left"
-                        }})
- 
+                { label: 'Coverage', fieldName: 'coverage', type: 'currency', cellAttributes: { alignment: 'left' } }
+            );
+
+            for (let index = 0; index < len; ++index) {
+                columns.push({
+                    label: products[index], type: 'cellselector',
+                    typeAttributes: {
+                        value: { fieldName: products[index] },
+                        labelkey: frequency,
+                        labeltype: "currency",
+                        checkedvariant: "brand-outline",
+                        uncheckedvariant: "base",
+                        checkedicon: "action:approval",
+                        uncheckedicon: "",
+                        lockwhenselected: true,
+                        cellallignment: "left"
+                    }
+                });
+            }
+            this.rateColumns = [...this.rateColumns, ...columns];
         }
-
-        this.rateColumns = [...this.rateColumns,...columns];
     }
 
-    }
-  
-    @api filterTable(product){           
-            this.rateColumns = [...this.rateColumns].filter(col => col.fieldName == product);
-          
-    }
-       
     handleRateSelection(event) {
-         
-        const value = {...event.detail.value};
+        const value = { ...event.detail.value };
         let rates = this.rateData;
-        const len = rates.length;
-    
+        const len = rates?.length;
 
         for (let i = 0; i < len; i++) {
-
-  
             //Updating the rateDate with the checked indicator.  Required when 
             //filtering products so checks display correctly in the grid.
-            if (rates[i].coverage == value.coverage){           
-                rates[i][value.productlabel] = {...value};
-                    
+            if (rates[i].coverage == value.coverage) {
+                rates[i][value.productlabel] = { ...value };
+
                 this.rateData = rates;
                 break;
-            
             }
-        } 
-
-        //Creates a message that is published to the Cart Channel.  This channel is listed to by the Rate iLHSaleCart LWC and creates
-        //a quote from this message.
-        let payload = {
-            productCode: value?.productcode,
-            paymentFrequency: 'Monthly',
-            billingMethod: 'ACH',
-            coverage: value?.coverage,
-            cost: value?.monthly
         }
-
-        publish(this.MessageContext, CART_CHANNEL, payload);
-    
     }
- 
-       
-    
-
 }
